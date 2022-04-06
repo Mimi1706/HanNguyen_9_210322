b8b7fc1d03516c56acd768aa86e0cd65bbbcdfbc
Mimi <hana.nguyen@outlook.fr>
2022-04-06 21:51:19 +0200

    Page erreur pour id erroné + boucle de tri data

--- src/Pages/HousingPage/HousingPage.js
+++ src/Pages/HousingPage/HousingPage.js
@@ -1,4 +1,5 @@
 import { Fragment, useEffect, useState } from "react"
+import { useParams } from "react-router-dom"
 import "./HousingPage.css"
 
 import Carousel from "../../Components/Carousel/Carousel"
@@ -6,58 +7,64 @@ import Tag from "../../Components/Tag/Tag"
 import Collapsible from "../../Components/Collapsible/Collapsible"
 import Rating from "../../Components/Rating/Rating"
 
+import ErrorPage from "../ErrorPage/ErrorPage"
+
 const HousingPage = () => {
-  const [data, setData] = useState([])
+  const [housing, setData] = useState([])
   let idURL = new URL(document.location).searchParams.get("id")
 
   useEffect(() => {
     fetch("./data.json")
       .then((response) => response.json())
       .then((responseData) => {
-        setData(responseData)
+        for (let i = 0; i < responseData.length; i++) {
+          if (responseData[i].id === idURL) {
+            setData(responseData[i])
+          }
+        }
       })
-  }, [])
+  }, [idURL])
 
   return (
     <main>
-      {data.map((housing) => {
-        return housing.id === idURL ? (
-          <Fragment key={housing.id}>
-            <Carousel src={housing.pictures} maxImg={housing.pictures.length} />
-            <section className="informations">
-              <h1 className="housingTitle">{housing.title}</h1>
-              <h2 className="housingLocation">{housing.location}</h2>
-              <div className="housingTagList">
-                {housing.tags.map((tag, index) => (
-                  <Tag tag={tag} key={index} />
-                ))}
-              </div>
-              <h3 className="housingHostName">{housing.host.name}</h3>
-              <img
-                src={housing.host.picture}
-                alt="Portrait de l'hôte"
-                className="housingHostPicture"
-              ></img>
-              <div className="housingRatingWrapper">
-                <Rating stars={housing.rating} />
-              </div>
-            </section>
-            <section className="collapsibleHousingWrapper">
-              <Collapsible label="Description" text={housing.description} />
-              <Collapsible
-                label="Équipements"
-                text={housing.equipments.map((equipement, index) => {
-                  return (
-                    <li className="housingEquipement" key={index}>
-                      {equipement}
-                    </li>
-                  )
-                })}
-              />
-            </section>
-          </Fragment>
-        ) : null
-      })}
+      {housing.id === idURL ? (
+        <Fragment key={housing.id}>
+          <Carousel src={housing.pictures} maxImg={housing.pictures.length} />
+          <section className="informations">
+            <h1 className="housingTitle">{housing.title}</h1>
+            <h2 className="housingLocation">{housing.location}</h2>
+            <div className="housingTagList">
+              {housing.tags.map((tag, index) => (
+                <Tag tag={tag} key={index} />
+              ))}
+            </div>
+            <h3 className="housingHostName">{housing.host.name}</h3>
+            <img
+              src={housing.host.picture}
+              alt="Portrait de l'hôte"
+              className="housingHostPicture"
+            ></img>
+            <div className="housingRatingWrapper">
+              <Rating stars={housing.rating} />
+            </div>
+          </section>
+          <section className="collapsibleHousingWrapper">
+            <Collapsible label="Description" text={housing.description} />
+            <Collapsible
+              label="Équipements"
+              text={housing.equipments.map((equipement, index) => {
+                return (
+                  <li className="housingEquipement" key={index}>
+                    {equipement}
+                  </li>
+                )
+              })}
+            />
+          </section>
+        </Fragment>
+      ) : (
+        <ErrorPage />
+      )}
     </main>
   )
 }
